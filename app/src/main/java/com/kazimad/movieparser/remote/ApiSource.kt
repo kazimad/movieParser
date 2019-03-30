b0e9e5a9a3a50698b98a7d41aa55346c25223957
kazimad <kazimad.kr@gmail.com>
2019-03-30 18:18:24 +0200

    cleanup

--- app/src/main/java/com/kazimad/movieparser/remote/ApiSource.kt
+++ app/src/main/java/com/kazimad/movieparser/remote/ApiSource.kt
@@ -1,8 +1,4 @@
 package com.kazimad.movieparser.remote
-
-//import io.reactivex.Observable
-//import io.reactivex.android.schedulers.AndroidSchedulers
-//import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 
@@ -10,16 +6,4 @@ class ApiSource @Inject constructor() {
 
     var apiInterface: ApiInterface? = null
         @Inject set
-
-//    fun getListWithData(releaseDate: String): Observable<List<MovieData>>? {
-//        return apiInterface!!.getList(Constants.API_KEY, Constants.API_SORT_BY, releaseDate)
-//            .filter(ApiHelper.baseApiFilterPredicate())
-//            .subscribeOn(Schedulers.io())
-//            .flatMap { getListMoviesFromResponse(it.body()!!) }
-//            .observeOn(AndroidSchedulers.mainThread())
-//    }
-
-//    private fun getListMoviesFromResponse(topResponse: TopResponse): Observable<List<MovieData>> {
-//        return Observable.fromArray(topResponse.results)
-//    }
 }
